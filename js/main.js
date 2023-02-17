b126cbeae15668a74dd23db9952f3d325a592821
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-02-17 15:49:07 +0100

    Complete ex.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,26 @@
+// Descrizione
+
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+
 // console.log("Vue OK", Vue);
 
-const app = Vue.createApp({
+const { createApp } = Vue;
+createApp({
   data() {
-    return {};
+    return {
+      emails: [],
+    };
   },
-});
 
-app.mount("#root");
+  mounted() {
+    for (let i = 0; i < 10; i++) {
+      axios
+        .get("https://flynn.boolean.careers/exercises/api/random/mail")
+        .then((response) => {
+          this.emails.push(response.data.response);
+        });
+    }
+  },
+}).mount("#root");
